8c602235e4f24855b1e0c3cbe77d704bbf3aa968
chumanstha <chumanashrestha@gmail.com>
2022-04-20 09:48:35 +0545

    manage router and imports

---
+++ src/components/WebRouter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes as Switch, Route, Navigate } from "react-router-dom";
+
+import Test from "./test";
+
+import Book from "./books/detail"
+import Addbook from "./books/add"
+import Editbook from "./books/edit"
+
+import User from "./users/detail"
+import Adduser from "./users/add"
+import Edituser from "./users/edit"
+
+export default function WebRouter(params) {
+    return (
+        <>
+            <Switch>
+                <Route path="/" element={<Navigate replace to="/dashboard" />} />
+                <Route path="book" element={<Book />} />
+                <Route path="book/add" element={<Addbook />} />
+                <Route path="book/edit" element={<Editbook />} />
+                <Route path="user" element={<User />} />
+                <Route path="user/add" element={<Adduser />} />
+                <Route path="user/edit" element={<Edituser />} />
+                <Route path="/*" element={<Test />} />
+            </Switch>
+        </>
+    );
+}
