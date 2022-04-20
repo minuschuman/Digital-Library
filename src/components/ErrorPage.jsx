ac61378f12ab50b93421f91c5ca12fa9408a768f
chumanstha <chumanashrestha@gmail.com>
2022-04-20 11:05:45 +0545

    style error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,17 @@
+import '../css/errorPage.css';
+import bulb from "../img/light-bulb.png"
 export default function ErrorPage(params) {
     return (
         <>
-            Page Not Found
+            <div className='errorBody'>
+                <img src={bulb} className="errImage" alt="404" />
+                <div>
+                    <div className="dialog">
+                        <h1>Hey, who turned off the lights?</h1>
+                        <p>We were unable to find the page you were looking for.</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
