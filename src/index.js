1425c77df367d5c517b91455ea0ce1ed32906dac
chumanstha <chumanashrestha@gmail.com>
2022-04-24 13:29:07 +0545

    change base url for deployed code

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// axios.defaults.baseURL = "https://minus-digitallibrary-api.herokuapp.com/api/";
-axios.defaults.baseURL = "http://127.0.0.1:5000/api/";
+axios.defaults.baseURL = "https://minus-digitallibrary-api.herokuapp.com/api/";
+// axios.defaults.baseURL = "http://127.0.0.1:5000/api/";
 // axios.defaults.baseURL = "http://192.168.137.1:5000/api/";
 root.render(
   <React.StrictMode>
